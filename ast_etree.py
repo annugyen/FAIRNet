4b8fba9a5b9ab933ad888edef7f1449a3465043a
AntelopeCub <rain.xu.1994@gmail.com>
2020-02-19 11:45:06 +0100

    extract most of layer parameters

--- ast_etree.py
+++ ast_etree.py
@@ -26,8 +26,8 @@ def list_to_tuple(tuple_list):
     tuple_list = eval(tuple_list)
     return tuple_list
 
-file_path = './francarranza_genre_classification.py'
-#file_path = './nagyben_CarND-Behavioral-Cloning-P3.py'
+#file_path = './francarranza_genre_classification.py'
+file_path = './nagyben_CarND-Behavioral-Cloning-P3.py'
 with open(file_path, 'r') as file:
     data = file.read()
 file.close()
@@ -56,6 +56,21 @@ if seqs:
         layer_num = 0
         seq_name = ''
         func = seq.xpath('ancestor::item[ast_type = "FunctionDef"]')
+        func_paras = func[0].xpath('child::args/args')[0].getchildren()
+        func_paras_list = []
+        if len(func_paras) > 0:
+            for func_para in func_paras:
+                func_paras_list.append(func_para.find('arg').text)
+        func_defaults = func[0].xpath('child::args/defaults')[0].getchildren()
+        func_defaults_dict = {}
+        if len(func_defaults) > 0:
+            for idx in range(len(func_defaults)):
+                if func_defaults[idx].find('ast_type').text == 'Num':
+                    func_defaults_dict[func_paras_list[len(func_paras) - len(func_defaults) + idx]] = func_defaults[idx].xpath('child::n/n')[0].text
+                elif func_defaults[idx].find('ast_type').text == 'Tuple':
+                    func_defaults_dict[func_paras_list[len(func_paras) - len(func_defaults) + idx]] = list_to_tuple(rebuild_tuple(func_defaults[idx]))
+                else:
+                    pass
         func_details = func[0].find('body').getchildren()
         for func_detail in func_details:
             ast_type = func_detail.find('ast_type')
@@ -80,11 +95,17 @@ if seqs:
                                 pass #todo
                             elif layer_para_type == 'Num':
                                 layer_paras_list.append(layer_para.xpath('child::n/n')[0].text)
+                            elif layer_para_type == 'Name':
+                                layer_para_id = layer_para.find('id').text
+                                if layer_para_id in func_defaults_dict:
+                                    layer_paras_list.append(func_defaults_dict[layer_para_id])
+                                else:
+                                    pass #todo find call para
                             else:
                                 pass
                         layer['parameters'] = layer_paras_list
                     layer_kws = value.xpath('child::args/item')[0].find('keywords')
-                    if layer_kws:
+                    if len(layer_kws) > 0:
                         for layer_kw in layer_kws:
                             layer_kw_key = layer_kw.xpath('child::arg')[0].text
                             layer_kw_value_type = layer_kw.xpath('child::value/ast_type')[0].text
@@ -93,7 +114,11 @@ if seqs:
                                 layer_kw_value = layer_kw.xpath('child::value/n/n')[0].text
                             elif layer_kw_value_type == 'Name':
                                 layer_kw_value = layer_kw.xpath('child::value/id')[0].text
-                                #todo: find parameters
+                                if layer_kw_value in func_defaults_dict:
+                                    layer_kw_value = func_defaults_dict[layer_kw_value] #todo
+                                    a = 1
+                                else:
+                                    pass #todo find call para
                             elif layer_kw_value_type == 'Str':
                                 layer_kw_value = layer_kw.xpath('child::value/s')[0].text
                             elif layer_kw_value_type == 'Tuple':
