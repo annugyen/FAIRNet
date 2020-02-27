716015d37e526d192dec8b2ca43594091d27517e
AntelopeCub <rain.xu.1994@gmail.com>
2020-02-27 17:28:29 +0100

    v4, almost done

--- ast_etree.py
+++ ast_etree.py
@@ -8,16 +8,37 @@ from json2xml import json2xml
 from json2xml.utils import readfromstring
 from lxml import etree
 
+global keras_apps_list
+keras_apps_list = ['Xception',
+                   'VGG16',
+                   'VGG19',
+                   'ResNet50',
+                   'ResNet101',
+                   'ResNet152',
+                   'ResNet50V2',
+                   'ResNet101V2',
+                   'ResNet152V2',
+                   'InceptionV3',
+                   'InceptionResNetV2',
+                   'MobileNet',
+                   'MobileNetV2',
+                   'DenseNet121',
+                   'DenseNet169',
+                   'DenseNet201',
+                   'NASNetMobile',
+                   'NASNetLarge']
 
-def rebuild_tuple(root):
-    rebuilt_tuple = []
+def rebuild_list(root):
+    rebuilt_list = []
     if root.find('ast_type').text == 'Num':
         return int(root.xpath('child::n/n')[0].text)
-    elif root.find('ast_type').text == 'Tuple':
+    elif root.find('ast_type').text == 'Str':
+        return root.find('s').text
+    elif root.find('ast_type').text in ['Tuple', 'List']:
         items = root.xpath('child::elts/item')
         for item in items:
-            rebuilt_tuple.append(rebuild_tuple(item))
-    return rebuilt_tuple
+            rebuilt_list.append(rebuild_list(item))
+    return rebuilt_list
 
 def list_to_tuple(tuple_list):
     tuple_list = str(tuple_list)
@@ -26,8 +47,18 @@ def list_to_tuple(tuple_list):
     tuple_list = eval(tuple_list)
     return tuple_list
 
-def get_func_call_paras_kws(root, is_def_func = False, **kwarg):
-    if not is_def_func:
+def rebuild_attr(root):
+    rebuilt_attr_list = []
+    while root.find('ast_type').text == 'Attribute':
+        rebuilt_attr_list.append(root.find('attr').text)
+        root = root.find('value')
+    if root.find('ast_type').text == 'Name':
+        rebuilt_attr_list.append(root.find('id').text)
+    rebuilt_attr = '.'.join(reversed(rebuilt_attr_list))
+    return rebuilt_attr
+
+def get_func_call_paras_kws(root, has_ext_paras = False, **kwarg):
+    if not has_ext_paras:
         func_paras_kws_dict = kwarg['func_paras_kws_dict']
         func_defaults_dict = kwarg['func_defaults_dict']
     func_call_paras = root.xpath('child::args/item')
@@ -41,7 +72,7 @@ def get_func_call_paras_kws(root, is_def_func = False, **kwarg):
                 func_call_paras_list.append(func_call_para.xpath('child::n/n')[0].text)
             elif func_call_para_type == 'Name':
                 func_call_para_name = func_call_para.find('id').text
-                if is_def_func:
+                if has_ext_paras:
                     func_call_paras_list.append(func_call_para_name)
                 elif func_call_para_name in func_paras_kws_dict:
                     func_call_paras_list.append(func_paras_kws_dict[func_call_para_name])
@@ -61,7 +92,7 @@ def get_func_call_paras_kws(root, is_def_func = False, **kwarg):
             if func_call_kw_value_type == 'Num':
                 func_call_kw_value = func_call_kw.xpath('child::value/n/n')[0].text
             elif func_call_kw_value_type == 'Name':
-                if is_def_func:
+                if has_ext_paras:
                     func_call_kw_value = func_call_kw.xpath('child::value/id')[0].text
                 elif func_call_kw_key in func_paras_kws_dict:
                     func_call_kw_value = func_paras_kws_dict[func_call_kw_key]
@@ -72,15 +103,26 @@ def get_func_call_paras_kws(root, is_def_func = False, **kwarg):
             elif func_call_kw_value_type == 'Str':
                 func_call_kw_value = func_call_kw.xpath('child::value/s')[0].text
             elif func_call_kw_value_type == 'Tuple':
-                func_call_kw_value = list_to_tuple(rebuild_tuple(func_call_kw.find('value')))
+                func_call_kw_value = list_to_tuple(rebuild_list(func_call_kw.find('value')))
+            elif func_call_kw_value_type == 'Attribute':
+                func_call_kw_value = rebuild_attr(func_call_kw.find('value'))
+            elif func_call_kw_value_type == 'List':
+                func_call_kw_value = rebuild_list(func_call_kw.find('value'))
+            elif func_call_kw_value_type == 'Call':
+                func_call_kw_value = func_call_kw.xpath('child::value/func/id')[0].text
+            elif func_call_kw_value_type == 'NameConstant':
+                func_call_kw_value = func_call_kw.xpath('child::value/value')[0].text
             else:
                 pass
             func_call_kws_dict[func_call_kw_key] = func_call_kw_value
     a = 1
     return func_call_paras_list, func_call_kws_dict
 
-file_path = './francarranza_genre_classification.py'
+#file_path = './francarranza_genre_classification.py'
+#file_path = './mcculzac_Volkswagen.py'
 #file_path = './nagyben_CarND-Behavioral-Cloning-P3.py'
+#file_path = './XintongHao_Self-Driving-Car-Behavioral-Cloning.py'
+file_path = './jw15_wildflower-finder.py'
 with open(file_path, 'r') as file:
     data = file.read()
 file.close()
@@ -100,53 +142,70 @@ models = {}
 model_num = 0
 
 seqs = code_tree.xpath('//func[id = "Sequential"]')
-if seqs:
+if len(seqs) > 0:
     for seq in seqs:
         model_num += 1
+        model_name = ''
+        models[model_num] = {}
         layers = {}
+        compile_kws_dict = {}
         layer_num = 0
-        seq_name = ''
+        seq_name = (seq.xpath('../..')[0]).xpath('child::targets/item/id')[0].text
+        is_model_compiled = False
         func = seq.xpath('ancestor::item[ast_type = "FunctionDef"]')
-        func_name = func[0].find('name').text
-        func_def_paras = func[0].xpath('child::args/args')[0].getchildren()
-        func_def_paras_list = []
-        if len(func_def_paras) > 0:
-            for func_def_para in func_def_paras:
-                func_def_paras_list.append(func_def_para.find('arg').text)
-        func_defaults = func[0].xpath('child::args/defaults')[0].getchildren()
-        func_defaults_dict = {}
-        if len(func_defaults) > 0:
-            for idx in range(len(func_defaults)):
-                if func_defaults[idx].find('ast_type').text == 'Num':
-                    func_defaults_dict[func_def_paras_list[len(func_def_paras) - len(func_defaults) + idx]] = func_defaults[idx].xpath('child::n/n')[0].text
-                elif func_defaults[idx].find('ast_type').text == 'Tuple':
-                    func_defaults_dict[func_def_paras_list[len(func_def_paras) - len(func_defaults) + idx]] = list_to_tuple(rebuild_tuple(func_defaults[idx]))
-                else:
-                    pass
-        #check if func is called
-        func_xpath = 'descendant::func[id="' + func_name + '"]'
-        func_call = code_tree.xpath(func_xpath)
-        is_func_called = False
-        if len(func_call) > 0:
-            is_func_called = True
-            func_call_root = func_call[0].xpath('..')[0]
-            func_call_paras_list, func_call_kws_dict = get_func_call_paras_kws(func_call_root, is_def_func = True)
+        if len(func) > 0:
+            model_in_func = True
+            func_name = func[0].find('name').text
+            func_def_paras = func[0].xpath('child::args/args')[0].getchildren()
+            func_def_paras_list = []
+            if len(func_def_paras) > 0:
+                for func_def_para in func_def_paras:
+                    func_def_paras_list.append(func_def_para.find('arg').text)
+            func_defaults = func[0].xpath('child::args/defaults')[0].getchildren()
+            func_defaults_dict = {}
+            if len(func_defaults) > 0:
+                for idx in range(len(func_defaults)):
+                    if func_defaults[idx].find('ast_type').text == 'Num':
+                        func_defaults_dict[func_def_paras_list[len(func_def_paras) - len(func_defaults) + idx]] = func_defaults[idx].xpath('child::n/n')[0].text
+                    elif func_defaults[idx].find('ast_type').text == 'Tuple':
+                        func_defaults_dict[func_def_paras_list[len(func_def_paras) - len(func_defaults) + idx]] = list_to_tuple(rebuild_list(func_defaults[idx]))
+                    else:
+                        pass
+            #check if func is called
+            func_xpath = 'descendant::func[id="' + func_name + '"]'
+            func_call = code_tree.xpath(func_xpath)
+            is_func_called = False
+            if len(func_call) > 0:
+                is_func_called = True
+                func_call_root = func_call[0].xpath('..')[0]
+                func_call_paras_list, func_call_kws_dict = get_func_call_paras_kws(func_call_root, has_ext_paras = True)
+                model_name = ((func_call_root.xpath('..')[0]).xpath('child::targets/item')[0]).xpath('descendant::id')[0].text
+            else:
+                func_call_paras_list = []
+                func_call_kws_dict = {}
+            if func_def_paras_list:
+                func_paras_dict = dict(zip(func_def_paras_list, func_call_paras_list))
+            else:
+                func_paras_dict = {}
+            func_paras_kws_dict = {**func_paras_dict, **func_call_kws_dict}
+            func_details = func[0].find('body').getchildren()
         else:
-            func_call_paras_list = []
-            func_call_kws_dict = {}
-        if func_def_paras_list:
-            func_paras_dict = dict(zip(func_def_paras_list, func_call_paras_list))
-        else:
-            func_paras_dict = {}
-        func_paras_kws_dict = {**func_paras_dict, **func_call_kws_dict}
-        func_details = func[0].find('body').getchildren()
+            model_in_func = False
+            func_details = code_tree.find('body')
+            model_name = seq_name
+            func_paras_kws_dict = {}
+            func_defaults_dict = {}
         for func_detail in func_details:
             ast_type = func_detail.find('ast_type')
             if ast_type.text == 'Assign':
-                seq_target = func_detail.find('targets')
-                seq_name = seq_target.xpath('descendant::id')[0].text
-                a = 1
-            elif ast_type.text == 'Expr':
+                if func_detail.xpath('child::value/ast_type')[0].text == 'Call':
+                    if func_detail.xpath('child::value/func/ast_type')[0].text == 'Attribute':
+                        base_model_name = func_detail.xpath('child::value/func/attr')[0].text
+                        if base_model_name in keras_apps_list:
+                            base_model_root = func_detail.find('value')
+                            base_model_paras, base_model_kws = get_func_call_paras_kws(base_model_root, func_paras_kws_dict = func_paras_kws_dict, func_defaults_dict = func_defaults_dict)
+                            models[model_num]['base_model'] = {'name':base_model_name, 'parameters': base_model_paras, **base_model_kws}
+            elif ast_type.text == 'Expr' and len(func_detail.xpath('child::value/func/attr')) > 0:
                 value = func_detail.find('value')
                 expr_func = value.find('func')
                 if expr_func.find('attr').text == 'add' and expr_func.xpath('child::value/id')[0].text == seq_name:
@@ -161,55 +220,73 @@ if seqs:
                     layer_paras_list, layer_kws_dict = get_func_call_paras_kws(layer_root, func_paras_kws_dict = func_paras_kws_dict, func_defaults_dict = func_defaults_dict)
                     layer['parameters'] = layer_paras_list
                     layer = {**layer, **layer_kws_dict}
-                    """
-                    if len(layer_paras) > 0:
-                        for layer_para in layer_paras:
-                            layer_para_type = layer_para.find('ast_type').text
-                            if layer_para_type == 'Lambda':
-                                pass #todo
-                            elif layer_para_type == 'Num':
-                                layer_paras_list.append(layer_para.xpath('child::n/n')[0].text)
-                            elif layer_para_type == 'Name':
-                                layer_para_id = layer_para.find('id').text
-                                if layer_para_id in func_paras_kws_dict:
-                                    layer_paras_list.append(func_paras_kws_dict[layer_para_id])
-                                elif layer_para_id in func_defaults_dict:
-                                    layer_paras_list.append(func_defaults_dict[layer_para_id])
+                    """ 
+                    #delete indentation if re-used
+                        if len(layer_paras) > 0:
+                            for layer_para in layer_paras:
+                                layer_para_type = layer_para.find('ast_type').text
+                                if layer_para_type == 'Lambda':
+                                    pass #todo
+                                elif layer_para_type == 'Num':
+                                    layer_paras_list.append(layer_para.xpath('child::n/n')[0].text)
+                                elif layer_para_type == 'Name':
+                                    layer_para_id = layer_para.find('id').text
+                                    if layer_para_id in func_paras_kws_dict:
+                                        layer_paras_list.append(func_paras_kws_dict[layer_para_id])
+                                    elif layer_para_id in func_defaults_dict:
+                                        layer_paras_list.append(func_defaults_dict[layer_para_id])
+                                    else:
+                                        layer_paras_list.append('not_found') #todo find other paras
                                 else:
-                                    layer_paras_list.append('not_found') #todo find other paras
-                            else:
-                                pass
-                        layer['parameters'] = layer_paras_list
-                    layer_kws = value.xpath('child::args/item')[0].find('keywords')
-                    if len(layer_kws) > 0:
-                        for layer_kw in layer_kws:
-                            layer_kw_key = layer_kw.xpath('child::arg')[0].text
-                            layer_kw_value_type = layer_kw.xpath('child::value/ast_type')[0].text
-                            layer_kw_value = None
-                            if layer_kw_value_type == 'Num':
-                                layer_kw_value = layer_kw.xpath('child::value/n/n')[0].text
-                            elif layer_kw_value_type == 'Name':
-                                layer_kw_value = layer_kw.xpath('child::value/id')[0].text
-                                if layer_kw_value in func_paras_kws_dict:
-                                    layer_kw_value = func_paras_kws_dict[layer_kw_value]
-                                    a = 1
-                                elif layer_kw_value in func_defaults_dict:
-                                    layer_kw_value = func_defaults_dict[layer_kw_value]
+                                    pass
+                            layer['parameters'] = layer_paras_list
+                        layer_kws = value.xpath('child::args/item')[0].find('keywords')
+                        if len(layer_kws) > 0:
+                            for layer_kw in layer_kws:
+                                layer_kw_key = layer_kw.xpath('child::arg')[0].text
+                                layer_kw_value_type = layer_kw.xpath('child::value/ast_type')[0].text
+                                layer_kw_value = None
+                                if layer_kw_value_type == 'Num':
+                                    layer_kw_value = layer_kw.xpath('child::value/n/n')[0].text
+                                elif layer_kw_value_type == 'Name':
+                                    layer_kw_value = layer_kw.xpath('child::value/id')[0].text
+                                    if layer_kw_value in func_paras_kws_dict:
+                                        layer_kw_value = func_paras_kws_dict[layer_kw_value]
+                                        a = 1
+                                    elif layer_kw_value in func_defaults_dict:
+                                        layer_kw_value = func_defaults_dict[layer_kw_value]
+                                    else:
+                                        layer_kw_value = 'not_found' #todo find other para
+                                elif layer_kw_value_type == 'Str':
+                                    layer_kw_value = layer_kw.xpath('child::value/s')[0].text
+                                elif layer_kw_value_type == 'Tuple':
+                                    layer_kw_value = list_to_tuple(rebuild_list(layer_kw.find('value')))
                                 else:
-                                    layer_kw_value = 'not_found' #todo find other para
-                            elif layer_kw_value_type == 'Str':
-                                layer_kw_value = layer_kw.xpath('child::value/s')[0].text
-                            elif layer_kw_value_type == 'Tuple':
-                                layer_kw_value = list_to_tuple(rebuild_tuple(layer_kw.find('value')))
-                            else:
-                                pass
-                            layer[layer_kw_key] = layer_kw_value
-                            a = 1
+                                    pass
+                                layer[layer_kw_key] = layer_kw_value
+                                a = 1
                     """
                     layers[layer_num] = layer
+                elif expr_func.find('attr').text == 'compile' and expr_func.xpath('child::value/id')[0].text == seq_name:
+                    is_model_compiled = True
+                    compile_root = expr_func.xpath('..')[0]
+                    _, compile_kws_dict = get_func_call_paras_kws(compile_root, has_ext_paras = True)
+                    models[model_num]['compile_info'] = compile_kws_dict
                 a = 1
             else:
                 pass
             a = 1
-        models[model_num] = layers
+        models[model_num]['layers'] = layers
+        if not is_model_compiled:
+            model_compiles = code_tree.xpath('//func[attr = "compile"]')
+            if len(model_compiles) > 0:
+                for model_compile in model_compiles:
+                    if model_compile.xpath('child::value/id')[0].text == model_name:
+                        is_model_compiled == True
+                        compile_root = model_compile.xpath('..')[0]
+                        _, compile_kws_dict = get_func_call_paras_kws(compile_root, has_ext_paras = True)
+                        models[model_num]['compile_info'] = compile_kws_dict
+                        break
+            else:
+                models[model_num]['compile_info'] = {}
 a = 1
