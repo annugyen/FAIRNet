9ab423577a38cb553eec6cc54e48ad3d0dc44516
BrokenHands <rain.xu.1994@gmail.com>
2020-04-28 00:19:38 +0200

    add loop in update.py

--- update.py
+++ update.py
@@ -17,45 +17,55 @@ def links_select(h5_files_links):
 
 if __name__ == '__main__':
     data_path = './data.json'
-    result_path = './result_data_h5.json'
+    #result_path = './result_data_h5.json'
     with open(data_path, 'r') as f:
         data_json = json.load(f)
     f.close()
-    results = {}
-    for idx in range(len(data_json)):
-        data = data_json[idx]
-        has_h5 = data['has_h5']
-        keras_used = data['keras_used']
-        if has_h5 and keras_used:
-            models = {}
-            model_num = 0
-            error_count = 0
-            h5_files_links = data['h5_files_links']
-            repo_full_name = data['repo_full_name']
-            result_dict = {'repo_full_name': repo_full_name}
-            if len(h5_files_links) > 1:
-                h5_files_links = links_select(h5_files_links)
-            for link in h5_files_links:
-                try:
-                    extracted_architecture, loss, optimizer, metrics, layers = extract_architecture_from_h5(link)
-                except Exception as e:
-                    error_count += 1
-                else:
-                    if extracted_architecture:
-                        model_num += 1
-                        if metrics[0] == 'loss':
-                            metrics = metrics[1:]
-                        models[model_num] = {'layers': layers,
-                                             'compile_info':{'optimizer': optimizer,
-                                                             'loss': loss,
-                                                             'metrics': metrics}}
-            if (not models) and error_count > 0:
-                models = 'Error'
-            result_dict['models'] = models
-            results[idx] = result_dict
-            print('%d: finish' % idx)
-            time.sleep(0.5)
-    result_json = json.dumps(results, indent = 4, separators = (',', ': '))
-    with open(result_path, 'w') as file:
-        file.write(result_json)
-    file.close()
+    block_size = 3000
+    block_num = len(data_json) // block_size + 1
+    for i in range(block_num):
+        result_path = './result_data_h5_part' + str('%02d' % i) + '.json'
+        results = {}
+        for j in range(block_size):
+            idx = i * block_size + j
+            #for idx in range(len(data_json)):
+            data = data_json[idx]
+            has_h5 = data['has_h5']
+            keras_used = data['keras_used']
+            if has_h5 and keras_used:
+                models = {}
+                model_num = 0
+                error_count = 0
+                h5_files_links = data['h5_files_links']
+                repo_full_name = data['repo_full_name']
+                result_dict = {'repo_full_name': repo_full_name}
+                if len(h5_files_links) > 1:
+                    h5_files_links = links_select(h5_files_links)
+                for link in h5_files_links:
+                    try:
+                        extracted_architecture, loss, optimizer, metrics, layers = extract_architecture_from_h5(link)
+                    except Exception as e:
+                        error_count += 1
+                    else:
+                        if extracted_architecture:
+                            model_num += 1
+                            if metrics[0] == 'loss':
+                                metrics = metrics[1:]
+                            models[model_num] = {'layers': layers,
+                                                 'compile_info':{'optimizer': optimizer,
+                                                                 'loss': loss,
+                                                                 'metrics': metrics}}
+                if (not models) and error_count > 0:
+                    models = 'Error'
+                result_dict['models'] = models
+                results[idx] = result_dict
+                print('%d: finish' % idx)
+                time.sleep(0.1)
+            if idx == len(data_json) - 1:
+                break
+        result_json = json.dumps(results, indent = 4, separators = (',', ': '))
+        with open(result_path, 'w') as file:
+            file.write(result_json)
+        file.close()
+        if idx == len(data_json) - 1:
+            break
